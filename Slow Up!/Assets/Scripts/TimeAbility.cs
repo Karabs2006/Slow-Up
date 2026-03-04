63237fa56af71acf1f57eb33e13375d22e2b52ef
Kayy <157222013+Karabs2006@users.noreply.github.com>
2026-03-04 13:47:41 +0200

    Fixing Time Mechanic Script
    - Fixed looping error when initiating time freeze
    - Added filling meter routine

---
+++ Slow Up!/Assets/Scripts/TimeAbility.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeAbility : MonoBehaviour
+{
+    public Slider slider; 
+    public FPController fPController;
+    bool isTimeUp= false;
+    bool isCountingDown = false;
+
+    bool isMeterFull = true;
+    
+
+    void Start()
+    {
+        slider.value = 5;
+    }
+
+
+    void Update()
+    {   
+        if(fPController.isTimeSlowed && !isCountingDown && isMeterFull)
+        {
+            StartCoroutine(SlowDown());
+        }
+
+        if (isTimeUp)
+        {
+            StopCoroutine(SlowDown());
+            isMeterFull = false;
+            StartCoroutine(FillMeter());
+            Time.timeScale = 1f;
+            isTimeUp = false;
+
+            if (isMeterFull)
+            {
+                StopCoroutine(FillMeter());
+            }
+        }
+  
+    }
+
+    IEnumerator SlowDown()
+    {   
+        isCountingDown = true;
+        fPController.isTimeSlowed = false;
+        for(int i = 5; i >= 0; i--)
+        {   
+            slider.value = i;
+
+            if(i == 0)
+            {
+            isTimeUp = true;
+            isCountingDown = false;
+            isMeterFull = false;
+            }
+
+            Time.timeScale = 0.3f;
+
+            yield return new WaitForSecondsRealtime(1f);
+        }
+        
+    }
+
+
+    IEnumerator FillMeter()
+    {   
+        for(int i = 0; i >= 0; i++)
+        {   
+            slider.value = i;
+
+            if(i == 5)
+            {
+                isMeterFull = true;
+            }
+
+            yield return new WaitForSecondsRealtime(1f);
+        }
+        
+    }
+
+}
